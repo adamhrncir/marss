aded5d42bccea619338fb82dea8f9ea5a8df65f5
Michal Dobiášovský <michal@dobiasovsky.net>
2023-05-22 17:18:16 +0200

    upraveno odesílání formulářů přes post + počasí

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -15,6 +15,7 @@ export default function Home() {
   
     <div className="col-sm-2">
       <div className="logo">
+      <Link href="/dashboard">
     <Image
         src="/images/logo.png"
         width={150}
@@ -22,6 +23,7 @@ export default function Home() {
         alt="logo"
         layout="responsive"
       />
+    </Link>
     </div>
     </div>
     <div className="col-sm-9">
@@ -46,7 +48,7 @@ export default function Home() {
 <h1>RSS Feed</h1>
 <p>To add more resources, enter the valid URL address and name</p>
 
-<form>
+<form action="/addRSS" method="post">
   <div className="form-group">
     <input type="url" className="form-control" id="URLadress"  placeholder="https://marss.cz/rss/ "/>
 <br></br>
@@ -79,7 +81,7 @@ export default function Home() {
 <h1>Password</h1>
 <p>To change your password write your old and new password</p>
 
-<form>
+<form action="/changePassword" method="post">
   <div className="form-group">
     <input type="password" className="form-control" id="oldPassword"  placeholder="Old password"/>
 <br></br>
@@ -87,6 +89,23 @@ export default function Home() {
     <br></br>
     <input type="password" className="form-control" id="newPasswordAgin"  placeholder="New password again"/>
   </div>
+  <br></br>
+  <button type="submit" className="btn btn-outline-light">Change password</button>
+
+
+</form>
+
+<hr></hr>
+
+<h1>Weather</h1>
+<p>To change your city to show weather write your new city</p>
+
+<form action="/setWeatherCity" method="post">
+  <div className="form-group">
+    <input type="text" className="form-control" id="city"  placeholder="Prague"/>
+    </div>
+  <br></br>
+  <button type="submit" className="btn btn-outline-light">Change city</button>
 
 
 </form>
