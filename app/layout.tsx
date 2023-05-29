eb3af4118df825ed299a5aa01f7686a9dfc890fd
Michal Dobiášovský <108874803+michaldobiasovsky@users.noreply.github.com>
2023-05-29 13:53:54 +0200

    Homepage (#8)
    * Vytvořeno základní rozhraní
    
    * Update README.md
    
    Proběhlo přejmenování projektu
    
    * Přidán background image
    
    * Nové rozhraní, Přidáno nové logo
    
    * přidána podpora fontů a proběhla oprava CSS stylů
    
    * Základní welcome page
    
    Odstraněny zbytečné soubory
    
    * Přidán odkaz na registraci

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import './globals.css'
 
 export const metadata = {
-  title: 'Apphub',
+  title: 'MARSS',
   description: 'The common place for your apps',
 }
 
 export default function RootLayout({ children }: any) {
   return (
     <html lang='en'>
+      <head>
+          <link rel="icon" href="/images/logo.png" />
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Source+Serif+Pro&display=swap" rel="stylesheet"></link>
+      </head>
       <body>
         <main>{children}</main>
       </body>
