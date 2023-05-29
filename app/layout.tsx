f9874800fdf97993972b6ab4b572d57d970dcc5f
Michal Dobiášovský <michal@dobiasovsky.net>
2023-05-29 16:20:14 +0200

    vytvořena login stránka

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: any) {
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Source+Serif+Pro&display=swap" rel="stylesheet"></link>
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
       </head>
       <body>
         <main>{children}</main>
---
+++ app/login/page.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Link from 'next/link'
+
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+<>
+<div className='main'>
+
+<div className="center">
+        <div className="image">
+      <Image
+        src="/images/logo.png"
+        width={150}
+        height={150}
+        alt="logo"
+        layout="responsive"
+      />
+      </div>
+      </div>
+
+<div className="register">
+
+<form>
+  <div className="form-group">
+    <label htmlFor="exampleInputEmail1">Email address</label>
+    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+  </div>
+  <div className="form-group">
+    <label htmlFor="exampleInputPassword1">Password</label>
+    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+  </div>
+  <br></br>
+  <div className="form-check">
+    <input type="checkbox" className="form-check-input" id="remember"/>
+    <label className="form-check-label" htmlFor="exampleCheck1">remember me</label>
+  </div>
+<br></br>
+<button type="button" className="btn btn-outline-light btn-lg" id="login">Login</button>
+</form>
+<br></br>
+<p className="center">Or you can use one of these login options</p>
+<div className="center">
+<Link href="/googleLogin">
+      <button type="button" className="btn btn-outline-light btn-lg"><i className="bi bi-google"></i></button>
+</Link>
+&nbsp;&nbsp;&nbsp;
+<Link href="/facebookLogin">
+      <button type="button" className="btn btn-outline-light btn-lg"><i className="bi bi-facebook"></i></button>
+</Link>
+&nbsp;&nbsp;&nbsp;
+<Link href="/githubLogin">
+      <button type="button" className="btn btn-outline-light btn-lg"><i className="bi bi-github"></i></button>
+</Link>
+
+</div>
+</div>
+
+</div>
+</>
+  )
+}
