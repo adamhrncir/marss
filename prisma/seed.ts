1fe8758097a2aa110d67fdd999565ca43f4a58ec
adamhrncir <adam@adamhrncir.cz>
2023-05-31 15:13:16 +0200

    seed change + fetchtime to Int

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 import { hash } from 'bcrypt'
+import { now } from 'next-auth/client/_utils'
 
 const prisma = new PrismaClient()
 
@@ -14,7 +15,35 @@ async function main() {
             password
         },
     })
+
+    const rss_content = await prisma.rss_content.upsert({
+        where: { url: 'www.superweb.cz/rss' },
+        update: {},
+        create: {
+            url: 'www.superweb.cz/rss',
+            title: 'Bába pod kořenem',
+            link: 'www.superweb.ct/clanky/babapodkorenem',
+            description: 'babka spadla pod koren. 40 minut bila do dreva, nez si ji nekdo vsimnul.',
+            guid: 'superweb.cz/123-456',
+            fetchtime: 1685538618,
+        }
+    })
+    
+    const rss = await prisma.rss.upsert({
+        where: { id: 1 },
+        update: {},
+        create: {
+            website_name: 'xd',
+            userId: 1,
+            rss_contentUrl: 'www.superweb.cz/rss'
+
+        },
+    })
+
+
     console.log({ user })
+    console.log({ rss })
+    console.log({ rss_content })
 }
 main()
     .then(() => prisma.$disconnect())
